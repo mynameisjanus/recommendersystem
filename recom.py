b3c1311a89888b33f1e53ee6155bad0e337b2ed5
mynameisjanus <janus_advincula@hotmail.com>
2019-12-18 21:27:09 +0800

    Update codes

--- recom.py
+++ recom.py
@@ -3,7 +3,7 @@ import h5py
 
 # First we need to load the data which is in matlab format
 dat = h5py.File('ratings.mat', 'r')
-ratings = f.get('data/variable')
+ratings = dat.get('data/variable')
 
 # Convert to numpy array
 ratings = np.array(ratings)
