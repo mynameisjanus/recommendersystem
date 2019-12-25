a44a8c48f6fc369366a7d60df325145b07e97785
mynameisjanus <janus_advincula@hotmail.com>
2019-12-25 19:32:54 +0800

    fixes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import scipy.io as sio
 import numpy as np
+import funcs
+import EM_algorithm
 
 # First we need to load the data which is in matlab format
 ratings = sio.loadmat('ratings.mat')
@@ -11,7 +13,7 @@ log = np.empty([4, 5])
 for i in range(4):
         for j in range(5):
                 K = i + 1
-                mixture, post = common.init(X, K, seed = j)
+                mixture, post = funcs.init(X, K, seed = j)
                 mix, post, loglike = naive_em.run(X, mixture, post)
                 log[i,j] = loglike
         funcs.plot(X, mix, post, "GMM with K=" + str(K)).savefig("GMM" + str(K) + ".png")
